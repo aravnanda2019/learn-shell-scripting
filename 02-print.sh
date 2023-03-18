da50452ec6b5bb5d7687e7c5bc455aee2131a803
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-18 13:47:32 +0530

    updating ./learn-shell-scripting

--- 02-print.sh
+++ 02-print.sh
@@ -18,10 +18,10 @@ echo Hello World
 
 echo -e "\e[31mHello in Red Color\e[0m"
 echo  Hello World
-
+echo kantamani
 
 # Sometimes we need to print multiple lines
 echo -e "line1\nline2\nline3"
 
-## \e for color; same \n for new line 
+## \e for color; same \n for new line
 
